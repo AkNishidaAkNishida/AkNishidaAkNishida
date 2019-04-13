a33bfa0b7a942fbbb51054ec1fa1ca437712e60e
AkNishida <46430194+AkNishida@users.noreply.github.com>
2019-04-14 05:55:31 +0900

    Update form3.php

--- form3.php
+++ form3.php
@@ -1,52 +1,46 @@
-<? php 
-//こちらの本を参考にしました↓
-//DESIGNMAP. PHP入門 確認画面付きのお問い合わせフォームをつくりながらPHPを学ぶ（第2版） (DESIGNMAP BOOKS) (Kindle の位置No.751-755). DESIGNMAP. Kindle 版. 
-
-session_start(); 
-if(isset($_POST['token'], $_SESSION['token']) && ($_POST['token'] === $_SESSION['token'])){ 
-    unset($_SESSION['token']); 
-    $name = $_SESSION['name']; 
-    $email = $_SESSION['email']; 
-    $subject = $_SESSION['subject']; 
-    $body = $_SESSION['body']; 
-    $dsn = 'mysql:dbname=contact_form;host=localhost;charset=utf8'; 
+<?php
+session_start();
+//form2.phpからPOSTメソッドで送られたワンタムチケットの値と、セッションに保存されているワンタイムチケットの値を比較
+if(isset($_POST['token'], $_SESSION['token']) && ($_POST['token'] === $_SESSION['token'])){
+    unset($_SESSION['token']);
+    $name = $_SESSION['name'];
+    $email = $_SESSION['email'];
+    $subject = $_SESSION['subject'];
+    $body = $_SESSION['body'];
+    
+    $dsn ='mysql:dbname=contact_form;host=localhost;charset=utf8'; 
     $user = 'root'; 
     $password = '';
-    $dbh = new PDO($dsn, $user, $password); 
-    $dbh->query('SET NAMES utf8'); 
-    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); 
-    $sql = 'INSERT INTO inquiries (name, email, subject, body) VALUES (?, ?, ?, ?)'; 
-    $stmt = $dbh->prepare($sql); 
-    $stmt->bindValue(1, $name, PDO::PARAM_STR); 
-    $stmt->bindValue(2, $email, PDO::PARAM_STR); 
-    $stmt->bindValue(3, $subject, PDO::PARAM_STR); 
-    $stmt->bindValue(4, $body, PDO::PARAM_STR); 
-    $stmt->execute(); 
-    $dbh = null; 
-    $_SESSION = array(); 
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params(); 
-        setcookie( session_name(), '', time() - 42000, 
-                  $params["path"], 
-                  $params["domain"], 
-                  $params["secure"], 
-                  $params["httponly"]
-                 ); 
-    } 
+    $dbh = new PDO($dsn, $user, $password);//new PODでクラスをインスタンス化
+    $dbh->query('SET NAMES utf8');// ->は、メソッドの呼び出し
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $sql = 'INSERT INTO inquiries (name, email, subject, body) VALUES (?, ?, ?, ?)';
+    //VALUES(?, ?, ?, ?)の?はプレースホルダ。SQLインジェクションを防ぐ。
+    $stmt = $dbh->prepare($sql);
+    //PDOSオブジェクトのprepareメソッド
+    $stmt->bindValue(1, $name, PDO::PARAM_STR);
+    //PDOStatementオブジェクトのbindValueメソッドを呼び出し、変数の値とプレースホルダを結び付けている
+    $stmt->bindValue(2, $email, PDO::PARAM_STR);
+    $stmt->bindValue(3, $subject, PDO::PARAM_STR);
+    $stmt->bindValue(4, $body, PDO::PARAM_STR);
     
-    session_destroy(); 
-
-}else{ 
-    header('Location:http://localhost/php_form/form1.php'); 
-    exit(); 
-} 
+    $stmt->execute();
+    //PDOStatementオブジェクトのexcuteメソッドを呼び出し、準備したSQLが実行される
+    
+    $dbh = null;//データベースと切断
+        
+}else{//直接、form3.phpにアクセスがあった場合は、form1.phpに切り替える
+    header('Location: http://localhost/php_form/form1.php');
+    exit();
+}
 ?>
-<! doctype html > 
-< html > 
-    < head >
-        < meta charset ="utf-8"> 
-        < title > 完了しました</ title >
-        </ head > < body > 
-    < p > どうもありがとうございます </ p >
-    </ body > 
-    </ html >
+
+<?doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>確認画面</title>
+    <body>
+    終了する場合は、ウィンドウを閉じてください。
+    </body>
+</html>
